5b2b36d3707ee125994de01c2fca5a8c5a8e866c
Mike Gabriel <mike.gabriel@das-netzwerkteam.de>
2024-02-04 12:24:50 +0100

    debian/maliit-framework.sh: Also set GTK_IM_MODULE/QT_IM_MODULE for Lomiri.

--- debian/maliit-framework.sh
+++ debian/maliit-framework.sh
@@ -1,7 +1,7 @@
-# Exports GTK_IM_MODULE and QT_IM_MODULE under Unity 8
+# Exports GTK_IM_MODULE and QT_IM_MODULE under Unity 8 / Lomiri
 
 case $XDG_SESSION_DESKTOP in
-  ubuntu-touch|unity8*)
+  ubuntu-touch|unity8*|lomiri)
     export GTK_IM_MODULE=maliit
     export QT_IM_MODULE=maliit
     ;;
